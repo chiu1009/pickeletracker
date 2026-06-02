1c5d7b83fba948cf201d6dfe73f504f5814e218f
chiu1009 <160579595+chiu1009@users.noreply.github.com>
2026-06-02 23:33:06 +0800

    Delete clicktracker directory

--- clicktracker/netlify/functions/track.js
+++
@@ -1,8 +0,0 @@
-export async function handler(event) {
-  const source = event.queryStringParameters.source || "unknown";
-
-  return {
-    statusCode: 200,
-    body: `Function working. Source = ${source}`
-  };
-}
